74520c3c64e595fbe885f57b4b05650d39e17e21
Alumno <Alumno@PC26>
2019-08-20 09:36:39 -0500

    v1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Actividad_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Imprime los numeros del x hasta el 60 de 3x3");
+            Console.WriteLine("De donde arrancamos el ciclo");
+            x = Convert.ToInt32(Console.ReadLine());
+            for (int i = x; i <= 60; i += 3)
+            {
+                Console.WriteLine("i =" + i);
+            }
+        }
+    }
+}
